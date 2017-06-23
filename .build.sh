f1a21fb92058b21084135af861db2d48c0793f60
Rahul Chatterjee <rahul@cs.cornell.edu>
2017-06-23 11:13:03 -0400

    change test scripts permission to the same as 'su' so that it can run the test_install tests

--- .build.sh
+++ .build.sh
@@ -15,4 +15,18 @@ if [[ $? != 0 ]]; then
     cmake ..
     make && make tests
     sudo make install
+    if [ "$(uname)" == "Darwin" ]; then
+        # Do something under Mac OS X platform
+        :
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        # Do something under GNU/Linux platform
+        sudo chown --reference=$(which su) ./test/tests
+        sudo chmod --reference=$(which su) ./test/tests
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        # Do something under 32 bits Windows NT platform
+        :
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+        # Do something under 64 bits Windows NT platform
+        :
+    fi
 fi
